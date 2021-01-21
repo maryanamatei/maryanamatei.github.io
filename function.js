3d1e4dde7e3ff70a90086b9c4c21353f093c0f47
Mariana M <matei.maryana@gmail.com>
2021-01-21 21:38:47 +0200

    Array map to find pageIds

--- function.js
+++ function.js
@@ -10,17 +10,20 @@ function hide(id){
 }   
 
 function show(id){
+    //console.info("show id=", id);
     document.getElementById(id).style.display = '';
 }
 
 function hideAllPages(){
-    var pages = document.querySelectorAll(".page");
-    for (var i = 0; i < pages.length;   i++) {
+    var pages = Array.from(document.querySelectorAll(".page"));
+    var pageIds = pages.map(function(page){
+        return page.id;
+    });
+    
+    for (var i = 0; i < pages.length; i++) {
         var page = pages[i]
         hide(page.id);
-       
     }
-
 }
 
 function hidePreviousPage(){
@@ -29,27 +32,37 @@ function hidePreviousPage(){
 
 function showPage(id){ 
     hideAllPages();
-    show(id);  
-    
+    show(id); 
 }
 
-function initMenu(){
-    console.warn('prepare click on links');
-    document.addEventListener("click",  function(e){
-        var link =e.target
-       if (link.matches("#top-menu-bar a")){
-           var id = link.getAttribute("data-page");
-           console.info("click", id);
-           showPage(id);
-           //link.classList.add("active");
-
-       }
+function listenMenuClicks(){
+    console.warn("not implemented --> ne plangem :)");
+    document.addEventListener("click", function (e){
+        var link = e.target;
+        //console.warn('link?', link.matches("#top-menu-bar a")); // zice daca am dat click pe linkuri
+        if (link.matches("#top-menu-bar a")) {
+            var id = link.innerHTML.toLowerCase();
+           // console.warn('click', id);
+            showPage(id);
+        }
         
-    })
-    
+    });
 }
 
-initMenu();
+listenMenuClicks(); //--> initializarea meniului
+
+//function initMenu(){
+  //  console.warn('prepare click on links');
+   // document.addEventListener("click",  function(e){
+       // var link =e.target
+       //if (link.matches("#top-menu-bar a")){
+          // var id = link.getAttribute("data-page");
+          // console.info("click", id);
+           //showPage(id);}
+    //})
+//}
+
+//initMenu();
 
 showPage(activePage); 
 
@@ -59,12 +72,6 @@ var skills = [
     "JS",
     "NodeJS"
 ];
-//dupa ce ducem skills in js -->vom itera elementele din acest array oricte ar fi ele in mod dinamic
-//var skillsLi_tmp = [
- //   "<li>" + skills[0]+ "</li>",
-  //  "<li>" + skills[0]+ "</li>",
-  //  "<li>" + skills[0]+ "</li>"
-//]
 
 var skillsLi = skills.map(function(skill){
     console.info(skill);
@@ -75,7 +82,7 @@ var skillsLi = skills.map(function(skill){
 var ul = document.querySelector("#skills ul");
 ul.innerHTML = skillsLi.join(" ");
 
-//ul.innerHTML = skillsLi[0]+ 
+ul.innerHTML = skillsLi[0]+ 
                skillsLi[1]+ 
                skillsLi[2];
 //document.querySelector("#skills ul").innerHTML = skills[2];
