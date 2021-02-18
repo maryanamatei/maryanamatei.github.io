4b7e10caa26eaaee22b1f175e0b3885d9088f67d
Mariana M <matei.maryana@gmail.com>
2021-02-18 20:28:52 +0200

    use arrow function instead of no name functions

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 var activePage = "skills";
 
 function hide(id){
-    var el =  document.getElementById(id);
+    const el =  document.getElementById(id);
     if (el) {
         el.style.display = "none";
     } else {
@@ -15,15 +15,15 @@ function show(id){
 
 function hideAllPages(){
     //hei, documentule, da-mi toate elementele care au clasa page
-    var pages = Array.from(document.querySelectorAll(".page"));
-    pages.forEach(function(page){
+    const pages = Array.from(document.querySelectorAll(".page"));
+    pages.forEach((page) => {
         hide(page.id);
     });
 }
 
 function hidePreviousPage(){
     hide(activePage);
-    var link = document.querySelector(`#top-menu-bar a[data-id="${activePage}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-id="${activePage}"]`);
     link.classList.remove("active");
 }
 
@@ -33,7 +33,7 @@ function showPage(id) {
 
     show(id);
 
-    var link = document.querySelector(`#top-menu-bar a[data-id="${id}"]`);
+    const link = document.querySelector(`#top-menu-bar a[data-id="${id}"]`);
     // console.info("activePage", activePage, "id", id);
     link.classList.add("active");
     activePage = id;
@@ -41,10 +41,10 @@ function showPage(id) {
 }
 
 function listenMenuClicks(){ // MenuInit grupa 5
-    document.addEventListener("click", function (e){
-        var link = e.target;
+    document.addEventListener("click", (e) => {
+        const link = e.target;
         if (link.matches("#top-menu-bar a")) {
-            var id = link.getAttribute("data-id");
+            const id = link.getAttribute("data-id");
             showPage(id);
         }
         
@@ -55,32 +55,45 @@ listenMenuClicks();
 
 showPage(activePage); 
 
-var allSkills = []; 
+let allSkills = []; 
 
 function showSkills(skills){
-    var allSkillsHtml = skills.map(function(skill){
-        var cls = skill.favorite ? "favorite" : "";
+    const allSkillsHtml = skills.map((skill) => {
+        const cls = skill.favorite ? "favorite" : "";
         return `<li class="${cls}">
         ${skill.name} <span> &middot(${skill.endorcements})</span>
         </li>`;
     });
     
-    var skillsEl = document.querySelector("#skills ul");
+    const skillsEl = document.querySelector("#skills ul");
     skillsEl.innerHTML = allSkillsHtml.join("");
 
 };
 
 fetch("data/skills.json")
-    .then(function(r) {
+    .then((r) => {
         return r.json();
     })
-    .then(function (skills){
-        skills.sort(function(s1,s2){
+    .then((skills) => {
+        skills.sort((s1,s2) => {
             return s2.endorcements - s1.endorcements;
-        })
+        });
         showSkills(skills);
     });
 
 showSkills(allSkills);
  
- 
+const karatisti = ["Evelina", "Laurentiu", "Nicoleta"];          
+console.info(karatisti);
+
+function collectFirstName(karatisti){
+    console.info("karatisti: ", karatisti.firstName);
+    if (!karatisti){
+        console.warn("please provide karatisti");
+    }
+    const firstNames = karatisti.map((karatist) => {
+        return karatisti.firstNames;
+    });
+    return firstNames;
+}
+collectFirstName(karatisti);
